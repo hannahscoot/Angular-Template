57ee89953b74e5e57c4e1a0eb00b452faede46ca
Hannah Scott <64924910+hannahscoot@users.noreply.github.com>
2023-02-24 11:37:05 +0000

    added ngrx
    Added state management, NgRx

--- ag/src/app/app.module.ts
+++ ag/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { DemoPageComponent } from './demo-page/demo-page.component';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { DemoPageComponent } from './demo-page/demo-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({}, {})
   ],
   providers: [],
   bootstrap: [AppComponent],
